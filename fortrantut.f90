9c0611984f07cbbdb5b4f439fc442162f525189b
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2022-08-30 23:50:54 +0700

    mins in video 20:42

--- fortrantut.f90
+++ fortrantut.f90
@@ -84,6 +84,40 @@ program fortrantut
     !print "(a10, f3.1)", "ATAN(0) = ", ATAN(0.0)
 
     !-- step 7 --
+    ! ==, /=, >, <, >=, <=  | .and. .or. .not.
+    !integer :: age = 16
+    !if ((age >= 5) .and. (age <= 6)) then
+    !    print *, "Kinderagrten"
+    !else if ((age >= 7) .and. (age <= 13)) then
+    !    print *, "Middle School"
+    !else if ((age >= 14) .and. (age <= 18)) then
+    !    print *, "High School"
+    !else
+    !    print *, "Stay home"
+    !end if 
+
+    !-- step 8 --
+    !print *, .true. .or. .false.
+    !print *, .not. .true.
+    !print *, 5 /= 9
+    !print *, "a" < "b"
+
+    !-- step 9 --
+    !integer :: age = 16
+    !select case(age)
+    !case (5)
+    !    print *, "Kindergarten"
+    !case (6:13)
+    !    print *, "Middle School"
+    !case (14, 15, 16, 17, 18)
+    !    print *, "High School"
+    !case default
+    !    print *, "Stay home"
+    !end select
+    
+
+
+
 
 
 end program fortrantut
