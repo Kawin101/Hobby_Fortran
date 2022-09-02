23137d25f09f46465354f12f498f687a05bccc46
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2022-09-02 21:26:18 +0700

    i am sleepy

--- fortrantut.f90
+++ fortrantut.f90
@@ -213,9 +213,25 @@ program fortrantut
     
     !print "(i3)", product(a1)
     ! "(i2)", sum(a1)
-    !-- end step 11.0 (29:29 min in video) --
+    !-- end step 11.0 (33:53 min in video) --
 
-
+    !-- do step 12 --
+    !integer :: num, cups
+    !real :: liters, quarts
+!
+    !do num = 1,12
+    !    print 100, num, num * 7
+    !    100 format(i2, ' * 7 = ', i3)
+    !end do
+!
+    !print "(/a18)", "Cups Liters Quarts"
+    !do cups = 1,10
+    !    liters = cups * .236
+    !    quarts = cups * .208
+    !    print 200, cups, liters, quarts
+    !    200 format(' ', i3, 2x, f5.3, 2x, f5.3)
+    !end do
+    !-- 36:45 in video -- 
 
 
 
